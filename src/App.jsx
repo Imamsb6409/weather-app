6bc1c5cf9dfefbb96bebe17a67e62c86d577d607
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2026-02-20 10:55:53 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import SideBar from './components/SideBar';
+import SideBar from './components/SideBarHuraaa';
 import WeatherCard from "./components/WeatherCard";
 import SpecialCard from './components/SpecialCard';
 import QueenCard from "./components/QueenCard";
